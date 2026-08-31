9c03cd9c875a3290d9b5c64c6bee5ad13358bf4c
gallaugher <john.gallaugher@bc.edu>
2026-08-31 11:55:52 -0400

    Initial Commit

---
+++ Great!/Great_App.swift
@@ -0,0 +1,17 @@
+//
+//  Great_App.swift
+//  Great!
+//
+//  Created by John Gallaugher on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct Great_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
